38ef32038084e79486529e99e6c6aad30514047e
Taylor <taylor.c.hansen@utah.edu>
2020-03-27 14:51:02 -0600

    Put liters parsing error log git commands at end of function (in case not needed)

--- py/uploadRepository.py
+++ py/uploadRepository.py
@@ -38,20 +38,6 @@ def uploadRepository(newsz, **kwargs):
 				crash_df.to_csv('errors/git_error_log.csv', mode='a', index=False, header=False)
 			else:
 				crash_df.to_csv('errors/git_error_log.csv', index=False)
-				
-	# Add and commit liters parsing log, too
-	try:
-		cmd.run("git add errors/liters_error_log.csv", check=True, shell=True)
-		litcont=1
-	except:
-		print("Couldn't add liters string parsing error log via git. Add manually.")
-		litcont = 0
-	if litcont:
-		try:
-			message = 'More liters string parsing errors added to error log'
-			cmd.check_call(['git'] + ['commit', '-m', f'{message}'])
-		except:
-			print("Couldn't commit liters string parsing error log via git. Commit manually.")
 
 	message = f'{str(datetime.now().strftime("%Y-%m-%d %H%M%S"))} - There are now {newsz} cars in all_cars.csv'
 
@@ -61,12 +47,6 @@ def uploadRepository(newsz, **kwargs):
 			cont = 1
 		except:
 			print('Committing crashed!')
-			# Save to error log
-			crash_df = pd.DataFrame({'timestamp':[str(datetime.now().strftime("%Y-%m-%d %H%M%S"))], 'message':["Git commit crash"]})
-			if os.path.isfile('errors/git_error_log.csv'):
-				crash_df.to_csv('errors/git_error_log.csv', mode='a', index=False, header=False)
-			else:
-				crash_df.to_csv('errors/git_error_log.csv', index=False)
 			cont = 0
 
 	if cont == 1:
@@ -75,12 +55,6 @@ def uploadRepository(newsz, **kwargs):
 			cont = 1
 		except:
 			print('Pulling crashed!')
-			# Save to error log
-			crash_df = pd.DataFrame({'timestamp':[str(datetime.now().strftime("%Y-%m-%d %H%M%S"))], 'message':["Git pull crash"]})
-			if os.path.isfile('errors/git_error_log.csv'):
-				crash_df.to_csv('errors/git_error_log.csv', mode='a', index=False, header=False)
-			else:
-				crash_df.to_csv('errors/git_error_log.csv', index=False)
 			cont = 0
 
 	if cont == 1:
@@ -89,14 +63,42 @@ def uploadRepository(newsz, **kwargs):
 			cont = 1
 		except:
 			print('Pushing crashed!')
-			# Save to error log
-			crash_df = pd.DataFrame({'timestamp':[str(datetime.now().strftime("%Y-%m-%d %H%M%S"))], 'message':["Git push crash"]})
-			if os.path.isfile('errors/git_error_log.csv'):
-				crash_df.to_csv('errors/git_error_log.csv', mode='a', index=False, header=False)
-			else:
-				crash_df.to_csv('errors/git_error_log.csv', index=False)
 			cont = 0
+		
+	# Add and commit liters parsing log, too
+	try:
+		cmd.run("git add errors/liters_error_log.csv", check=True, shell=True)
+		litcont=1
+	except:
+		print("Couldn't add liters string parsing error log via git. Add manually.")
+		litcont = 0
+		
+	if litcont:
+		try:
+			liters_message = 'More liters string parsing errors added to error log'
+			cmd.check_call(['git'] + ['commit', '-m', f'{liters_message}'])
+			litcont = 1
+		except:
+			print("Couldn't commit liters string parsing error log via git. Commit manually (if needed).")
+			litcont = 0
+	
+	if litcont:
+		try:
+			cmd.run("git pull project master", check=True, shell=True)
+			litcont = 1
+		except:
+			print('Pulling crashed!')
+			litcont = 0
 
+	if litcont:
+		try:
+			cmd.run("git push project master", check=True, shell=True)
+			litcont = 1
+		except:
+			print('Pushing crashed!')
+			litcont = 0
+			
+			
 	if cont == 1:
 		print()
 		print(message)
