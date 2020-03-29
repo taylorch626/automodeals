b722d4cf36983d6fce89a67f5f383823d603580d
Michael Paskett - Lab <michael.paskett@utah.edu>
2020-03-28 22:48:17 -0600

    Wrapper for view updater and bug fixes

--- py/Favorites_views_updater.py
+++ py/Favorites_views_updater.py
@@ -8,7 +8,7 @@ Mostly copied from jupyter notebook of same name 3/28/20
 REQUIRED INPUTS:
 cars_df: data frame with information as pulled from carscraper()
 VARIABLE INPUTS:
-min_age: int specifying minimum age (days) listing must be before updating information. Default 3
+min_age: int specifying minimum age (days) listing must be before updating information. Default 1
 min_last_pull: int specifying minimum time (days) since last pull for new information. Default 1
 use_proxy: bool indicating to use a proxy. Default 0
 proxy_dict: dictionary of proxy IPs and user agents'''
@@ -22,12 +22,12 @@ import numpy as np
 import requests
 import progressbar
 from bs4 import BeautifulSoup
-from datetime import datetime
+import datetime
 
 # Import functions
 from generateProxies import generateProxies
 
-def update_listing_info(cars_df, **kwargs):
+def favorites_views_updater(cars_df, **kwargs):
     '''Updates a cars_dataframe with 6 new columns (views, favorites, 
     workingURL, view_rate, favorite_rate, fav_per_view). 
     REQUIRED INPUTS:
@@ -45,7 +45,7 @@ def update_listing_info(cars_df, **kwargs):
         else:
             raise TypeError(f'Expected int for min_age but got {type(kwargs["min_age"])}.')
     else:
-        min_age = 3 # days
+        min_age = 1 # days
         
     if 'min_last_pull' in kwargs.keys():
         if isinstance(kwargs['min_last_pull'],int):
@@ -110,13 +110,19 @@ def update_listing_info(cars_df, **kwargs):
         proxy_pool = cycle(proxydict) # make a pool of proxies 
         currproxy = next(proxy_pool) # grab the next proxy in cycle     
 
-    # new columns to add
-    cars_df['views'] = np.NaN
-    cars_df['favorites'] = np.NaN
-    cars_df['workingURL'] = 1
-    cars_df['view_rate'] = np.NaN
-    cars_df['favorite_rate'] = np.NaN
-    cars_df['fav_per_view'] = np.NaN
+    # new columns to add if not already there
+    if not 'views' in cars_df.columns:
+        cars_df['views'] = np.NaN
+    if not 'favorites' in cars_df.columns:
+        cars_df['favorites'] = np.NaN
+    if not 'workingURL' in cars_df.columns:
+        cars_df['workingURL'] = 1
+    if not 'view_rate' in cars_df.columns:
+        cars_df['view_rate'] = np.NaN
+    if not 'favorite_rate' in cars_df.columns:
+        cars_df['favorite_rate'] = np.NaN
+    if not 'fav_per_view' in cars_df.columns:
+        cars_df['fav_per_view'] = np.NaN
     
     # conversions to datetime
     orig_dates = cars_df['post_date']
@@ -140,54 +146,20 @@ def update_listing_info(cars_df, **kwargs):
     
     # test if IP isn't blocked with generic cars.ksl.com
     if not use_proxy:
-        resp = requests.get('cars.ksl.com', headers = {'User-Agent'})
+        resp = requests.get('https://cars.ksl.com', headers = {'User-Agent': user_agent})
         if resp.status_code == 403: # blocked
             print('IP Blocked. Continuing with proxies')
             use_proxy = 1
     
-    # iterate through, pulling new information from each ad
-    last_pull = []
-    views = []
-    favorites = []
-    working_url = []
-    with progressbar.ProgressBar(max_value=len(cars_need_update.index)) as bar:
-        for i, ad in cars_need_update.iterrows():
-            if use_proxy:
-                attempts = len(proxydict)
-                chkproxy = 1
-                while chkproxy:
-                    if (time.time() - tstart) > 60*refreshmin: # check if it's been more than refreshmin minutes since proxy_pool updated
-                        print('Refreshing proxy pool...')
-                        tstart = time.time()
-
-                        currproxies = set(proxydict.keys())
-                        newproxies = generateProxies()
-                        newproxies = newproxies.difference(currproxies)
-
-                        if newproxies:
-                            newdict = {i:random.choice(user_agents) for i in newproxies}
-                            proxydict.update(newdict)
-                            proxy_pool = cycle(proxydict)
-                            currproxy = next(proxy_pool)
-                            print('Proxy pool updated!')
-
-                    try:
-                        ad_response = requests.get(ad['link'],proxies={"http":currproxy, "https":currproxy},headers={'User-Agent': proxydict[currproxy]}, timeout=20)
-                        print(f'Proxy success for {currproxy}')
-                        print()
-                        chkproxy = 0
-                        attempts += 1
-                    except:
-                        prevproxy = currproxy
-                        currproxy = next(proxy_pool)
-                        print(f'Proxy error for {prevproxy}! Next up is {currproxy}')
-                        attempts -= 1
-                        print(f'Attempts remaining: {attempts}')
-            else:
-                ad_response = requests.get(ad['link'], headers = {'User-Agent': user_agent})
-                if ad_response.status_code == '403':
-                    print('IP was just blocked. Running with proxies')
-                    use_proxy = 1
+    if cars_need_update.shape[0] != 0:
+        # iterate through, pulling new information from each ad
+        last_pull = []
+        views = []
+        favorites = []
+        working_url = []
+        with progressbar.ProgressBar(max_value=len(cars_need_update.index)) as bar:
+            for i, ad in cars_need_update.iterrows():
+                if use_proxy:
                     attempts = len(proxydict)
                     chkproxy = 1
                     while chkproxy:
@@ -217,40 +189,77 @@ def update_listing_info(cars_df, **kwargs):
                             currproxy = next(proxy_pool)
                             print(f'Proxy error for {prevproxy}! Next up is {currproxy}')
                             attempts -= 1
-                            print(f'Attempts remaining: {attempts}')     
-                            
-            pull_ts = pd.to_datetime(time.time(), unit='s')
-            last_pull.append(pull_ts)
-            ad_soup = BeautifulSoup(ad_response.content)
-
-            # Check if link is still good (i.e. listing is still active)
-            if ad_soup.title.text.strip().lower() == 'not found':
-                working_url.append(0)
-                views.append(None)
-                favorites.append(None)
-            else:
-                working_url.append(1)
-
-                # get views
-                viewcount = int(ad_soup.select('span.vdp-info-value')[1].text.split()[0])
-                views.append(viewcount)
-
-                # get favorites
-                favoritecount = int(ad_soup.select('span.vdp-info-value')[2].text.split()[0])
-                favorites.append(favoritecount)
-            bar.update(i)
-            
-    cars_updated = cars_need_update
-    cars_updated['views'] = views
-    cars_updated['favorites'] = favorites
-    cars_updated['lastpull_ts'] = last_pull
-    cars_updated['workingURL'] = working_url
-    cars_updated['fav_per_view'] = cars_updated['favorites'] / cars_updated['views']
-    # rates calculated per day
-    cars_updated['view_rate'] = cars_updated['views'] / ((cars_updated['lastpull_ts'] - cars_updated['post_date']).dt.total_seconds()*60*60*24)
-    cars_updated['favorite_rate'] = cars_updated['favorites'] / ((cars_updated['lastpull_ts'] - cars_updated['post_date']).dt.total_seconds()*60*60*24)
-
-    cars_df.update(cars_updated)
+                            print(f'Attempts remaining: {attempts}')
+                else:
+                    ad_response = requests.get(ad['link'], headers = {'User-Agent': user_agent})
+                    if ad_response.status_code == '403':
+                        print('IP was just blocked. Running with proxies')
+                        use_proxy = 1
+                        attempts = len(proxydict)
+                        chkproxy = 1
+                        while chkproxy:
+                            if (time.time() - tstart) > 60*refreshmin: # check if it's been more than refreshmin minutes since proxy_pool updated
+                                print('Refreshing proxy pool...')
+                                tstart = time.time()
+        
+                                currproxies = set(proxydict.keys())
+                                newproxies = generateProxies()
+                                newproxies = newproxies.difference(currproxies)
+        
+                                if newproxies:
+                                    newdict = {i:random.choice(user_agents) for i in newproxies}
+                                    proxydict.update(newdict)
+                                    proxy_pool = cycle(proxydict)
+                                    currproxy = next(proxy_pool)
+                                    print('Proxy pool updated!')
+        
+                            try:
+                                ad_response = requests.get(ad['link'],proxies={"http":currproxy, "https":currproxy},headers={'User-Agent': proxydict[currproxy]}, timeout=20)
+                                print(f'Proxy success for {currproxy}')
+                                print()
+                                chkproxy = 0
+                                attempts += 1
+                            except:
+                                prevproxy = currproxy
+                                currproxy = next(proxy_pool)
+                                print(f'Proxy error for {prevproxy}! Next up is {currproxy}')
+                                attempts -= 1
+                                print(f'Attempts remaining: {attempts}')     
+                                
+                pull_ts = pd.to_datetime(time.time(), unit='s')
+                last_pull.append(pull_ts)
+                ad_soup = BeautifulSoup(ad_response.content)
+    
+                # Check if link is still good (i.e. listing is still active)
+                if ad_soup.title.text.strip().lower() == 'not found':
+                    working_url.append(0)
+                    views.append(None)
+                    favorites.append(None)
+                else:
+                    working_url.append(1)
+    
+                    # get views
+                    viewcount = int(ad_soup.select('span.vdp-info-value')[1].text.split()[0])
+                    views.append(viewcount)
+    
+                    # get favorites
+                    favoritecount = int(ad_soup.select('span.vdp-info-value')[2].text.split()[0])
+                    favorites.append(favoritecount)
+                bar.update(i)
+                
+        cars_updated = cars_need_update
+        cars_updated['views'] = views
+        cars_updated['favorites'] = favorites
+        cars_updated['lastpull_ts'] = last_pull
+        cars_updated['workingURL'] = working_url
+        cars_updated['fav_per_view'] = cars_updated['favorites'] / cars_updated['views']
+        # rates calculated per day
+        cars_updated['view_rate'] = cars_updated['views'] / ((cars_updated['lastpull_ts'] - cars_updated['post_date']).dt.total_seconds()*60*60*24)
+        cars_updated['favorite_rate'] = cars_updated['favorites'] / ((cars_updated['lastpull_ts'] - cars_updated['post_date']).dt.total_seconds()*60*60*24)
+    
+        cars_df.update(cars_updated)
+    else:
+        print(f'No cars to update based on min_age = {min_age} day(s) and min_last_pull = {min_last_pull} day(s).')
     
     # update timestamps to replicate original state
     cars_df['lastpull_ts'] = (cars_df['lastpull_ts'] - datetime.datetime(1970,1,1)).dt.total_seconds().astype(int)
